775b6c402e1bdebd46d56b7900a4c91b4ef46ff9
lodestone-rock <lodestone.rock@gmail.com>
2023-05-22 06:21:48 +0000

    meta tag stripping

--- csv_tag_parser.py
+++ csv_tag_parser.py
@@ -8,11 +8,25 @@ import os
 debug = True
 tag_index_test = True
 debug_random_seed = 42
+enable_tag_threshold = False
+# set this to 0 if you want to keep everything
+low_tag_count_threshold = 10
+low_artist_post_count_threshold = 10
+stripped_meta_tags = [
+    "description",
+    "text",
+    "audio",
+    "version",
+    "\d+:\d+",
+    "^\d{4}$",
+    r"\bsource\b",
+]
 
 
 def create_tag_index(df: pd.DataFrame, col_id: str, tag_col: str) -> pd.DataFrame:
     """
     Create a tag index DataFrame based on the provided DataFrame.
+    can also be used to revert it back to tag list
 
     Args:
         df (pd.DataFrame): The input DataFrame containing the data.
@@ -24,11 +38,16 @@ def create_tag_index(df: pd.DataFrame, col_id: str, tag_col: str) -> pd.DataFram
 
     Example:
         >>> df = pd.DataFrame({'id': [1, 2, 3], 'tags': [['tag1', 'tag2'], ['tag2', 'tag3'], None]})
-        >>> create_tag_index(df, 'id', 'tags')
+        >>> df_tags = create_tag_index(df, 'id', 'tags')
+        >>> df_tags
         tag_col  col_id
         tag1     [1]
         tag2     [1, 2]
         tag3     [2]
+        >>> create_tag_index(df_tags, 'id', 'tags')
+        id  tags
+        1  [tag1, tag2]
+        2  [tag2, tag3]
     """
     tag_index = {}
 
@@ -81,6 +100,9 @@ def separate_tag_string_as_comma_separated(
 
 
 def main():
+    # NOTE: this entire function here is to "strips" unnecessary tags NOT
+    # to delete, filter, or drop post
+
     # relative current dir of this script
     current_dir = os.getcwd()
 
@@ -95,15 +117,61 @@ def main():
     if debug:
         e6_dump = e6_dump.sample(n=10**4, random_state=debug_random_seed)
 
+    # strip underscores and convert tag string into a list of tags for each post
     e6_dump = separate_tag_string_as_comma_separated(df=e6_dump, tag_col="tag_string")
-
+    # strips underscores from tags
+    tag_lut["name"] = tag_lut["name"].str.replace("_", " ")
     print()
 
-    # filter artist that less than 20 post
-    tag_lut = tag_lut[tag_lut["category"] == 1][tag_lut["post_count"] > 20]
-
     # this indexing is slow, you might want to run this on multiple cores
+    # or not? this just took like 10 minutes to run through all 4m rows
     tag_to_index = create_tag_index(df=e6_dump, col_id="id", tag_col="tag_string")
+
+    # count index occurrence in the e6 database dump
+    tag_to_index["tag_count"] = tag_to_index["id"].apply(len)
+
+    # strip tags that below certain threshold
+    # disable this during debugging because artist tags
+    # will have low tag count
+    if enable_tag_threshold:
+        # filter rare tags
+        tag_to_index = tag_to_index[tag_to_index["tag_count"] > low_tag_count_threshold]
+        tag_lut = tag_lut[tag_lut["post_count"] > low_tag_count_threshold]
+
+    # ===[get artist name and prepend "by"]=== #
+
+    # store list of artist name
+    list_of_artist = tag_lut.loc[tag_lut["category"] == 1, "name"].to_list()
+
+    # boolean mask to select all artist tags
+    select_artist = tag_to_index["tag_string"].isin(list_of_artist)
+
+    # {artist_name} >> "by {artist_name}"
+    tag_to_index.loc[select_artist, "tag_string"] = (
+        "by " + tag_to_index.loc[select_artist, "tag_string"]
+    )
+
+    # ===[strip lore tag]=== #
+    # get lore tag from tag_lut
+    lore_tag = tag_lut[tag_lut.category == 8]["name"].to_list()
+    #  exclude lore tag
+    tag_to_index = tag_to_index[~tag_to_index["tag_string"].isin(lore_tag)]
+
+    # ===[bulk strip meta tags]=== #
+    meta_tags = tag_lut[tag_lut["category"] == 7]
+
+    for stripped_meta_tag in stripped_meta_tags:
+        meta_tags = meta_tags[~meta_tags["name"].str.contains(stripped_meta_tag)]
+
+    # select inverse of the meta tags
+    dropped_meta_tags = tag_lut[tag_lut["category"] == 7]
+    dropped_meta_tags = dropped_meta_tags[
+        ~dropped_meta_tags["name"].isin(meta_tags["name"].to_list())
+    ]
+    dropped_meta_tags = dropped_meta_tags["name"].to_list()
+
+    #  exclude some meta tags
+    tag_to_index = tag_to_index[~tag_to_index["tag_string"].isin(dropped_meta_tags)]
 
     # run test to compare if original data is equal after index transformation
     if tag_index_test:
@@ -131,7 +199,7 @@ def main():
 
     e6_dump = e6_dump.set_index("id")
 
-    # concat
+    # concat as new column so the original column stays and can be used for cross checking
     e6_dump = pd.concat([e6_dump, index_to_tag], axis=1)
 
     print()
